898692fbcca02185320fc401e8eb4f3b78e8f3e3
Benjamin Stookey <ben.stookey@gmail.com>
2015-04-16 08:17:24 -0700

    Some changes to get test coverage working locally;

--- wct.conf.js
+++ wct.conf.js
@@ -1,9 +1,18 @@
 module.exports = {
   verbose: false,
   plugins: {
+
     local: {
       disabled: false,
-      browsers: ['firefox']
+      browsers: ['firefox'],
+    },
+
+    "web-component-tester-istanbul": {
+      disabled: true,
+      dir: "./coverage",
+      reporters: ["lcov"],
+      include: [],
+      exclude: []
     }
   }
 };
